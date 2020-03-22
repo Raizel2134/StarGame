a2b17e9b217d57871749f5399856cb73d033fbaf
Raizel2134 <44531067+Raizel2134@users.noreply.github.com>
2020-03-22 17:32:18 +0300

    Попытался сделать отрисовку фона в зависимости от размеров экрана. Но в app приложение не корректно измеряется высота.

--- core/src/com/mygdx/game/Star.java
+++ core/src/com/mygdx/game/Star.java
@@ -4,16 +4,29 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Star extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	static Sprite backSprite;
+
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("space.jpg");
+		//Определяем размер приложения, для корректной отрисовки фона.
+		// В app приложении, высоты определяется не совсем корректно.
+		// Она в половину меньше высоты экрана.
+		Gdx.app.getGraphics();
+		int width = Gdx.app.getGraphics().getWidth();
+		int height = Gdx.app.getGraphics().getHeight();
+		backSprite = new Sprite(img);
+		//Изменение высоты и ширины фона.
+		backSprite.setSize(width, height);
 	}
 
 	@Override
